81da220b547aabf28452b20e9c370b96200af5ea
Danil Ermolin <thedanstrongny42@gmail.com>
2024-07-09 21:45:16 -0400

    Second commit

--- src/LoginPage.java
+++ src/LoginPage.java
@@ -1,4 +1,5 @@
 import java.util.HashMap;
+import java.awt.Font;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
@@ -17,21 +18,34 @@ public class LoginPage implements ActionListener {
     JButton resetButton = new JButton("Login");
     JTextField userIDField = new JTextField();
     JPasswordField userPasswordField = new JPasswordField();
-    JLabel userIDLabel = new JLabel("User ID");
+    JLabel userIDLabel = new JLabel("User ID: ");
     JLabel userPassworLabel = new JLabel("Password: ");
-    JLabel msgLabel = new JLabel(); //Success label
+    JLabel msgLabel = new JLabel(""); //Success label
     HashMap<String,String> logininfo = new HashMap<String,String>();
 
     LoginPage(HashMap<String,String> OrigLoginInfo){
 
         logininfo = OrigLoginInfo;
 
-        userIDLabel.setBounds(50,90,75,25);
-        userPassworLabel.setBounds(50,120,75,25);
+
+        //Setting bound for User ID label and Password label
+        userIDLabel.setBounds(65,99,75,25);
+        userPassworLabel.setBounds(50,150,75,25);
+
+        msgLabel.setBounds(125,250,250,35);
+        msgLabel.setFont(new Font(null,Font.ITALIC,25));
+
+        //User ID field and Password field
+
+        userIDField.setBounds(125,100,200,25);
+        userPasswordField.setBounds(125, 150, 200, 25);
 
 
         frame.add(userIDLabel);
         frame.add(userPassworLabel); 
+        frame.add(msgLabel);
+        frame.add(userIDField);
+        frame.add(userPasswordField);
 
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setSize(390,390);
