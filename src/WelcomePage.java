69ec522a40974b7f27b8ec6fcd291d7996479bac
Danil Ermolin <thedanstrongny42@gmail.com>
2024-07-11 20:12:14 -0400

    Added title and Icon Image to the welcome page

--- src/WelcomePage.java
+++ src/WelcomePage.java
@@ -7,15 +7,21 @@ import javax.swing.JLabel;
 public class WelcomePage {
 
     JFrame frame = new JFrame();
+    ImageIcon image = new ImageIcon("src/7067965.png");
     JLabel welcomeLabel = new JLabel("Welcome!");
 
 
+
     WelcomePage(String userID){
+
+        frame.setTitle("Welcome Page");
+        frame.setIconImage(image.getImage());
 
         welcomeLabel.setBounds(0,0,200,35);
         welcomeLabel.setFont(new Font(null, Font.PLAIN,25));
         welcomeLabel.setText("Hello, " + userID);
 
+        
         frame.add(welcomeLabel);
 
 
