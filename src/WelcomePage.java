c5471059f2471762e9054cec1ed29a4a91232837
Danil Ermolin <thedanstrongny42@gmail.com>
2024-07-12 20:08:48 -0400

    Attempting to add picture to WelcomePage frame

--- src/WelcomePage.java
+++ src/WelcomePage.java
@@ -1,15 +1,28 @@
+import java.awt.Component;
 import java.awt.Font;
+import java.awt.Image;
+import java.io.File;
 
+import javax.imageio.ImageIO;
 import javax.swing.ImageIcon;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 
 public class WelcomePage {
 
+
+
+    
     JFrame frame = new JFrame();
     ImageIcon image = new ImageIcon("src/7067965.png");
-    JLabel welcomeLabel = new JLabel("Welcome!");
 
+    ImageIcon catImage = new ImageIcon("src/CatPicture.jpg");
+    JLabel cat = new JLabel(catImage);
+    
+    JLabel welcomeLabel = new JLabel("Welcome!");
+    
+
+    
 
 
     WelcomePage(String userID){
@@ -21,14 +34,17 @@ public class WelcomePage {
         welcomeLabel.setFont(new Font(null, Font.PLAIN,25));
         welcomeLabel.setText("Hello, " + userID);
 
-        
+
+        frame.add(cat);
         frame.add(welcomeLabel);
+        
 
 
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setSize(420,420);
         frame.setLayout(null);
         frame.setVisible(true);
+        frame.pack();
     }
 
 }
